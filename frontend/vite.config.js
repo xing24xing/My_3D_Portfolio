675e96db2c46d15f1c59e6323f758aff5fd677f5
xing24xing <khushipandey260304@gmail.com>
2024-12-06 19:23:26 +0530

    Configure deploy error

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // build output directory
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0] // better chunking for node_modules
+          }
+        }
+      }
+    }
+  }
 })
